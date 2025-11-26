cc62d2d286203d399a9cf906fb615b7f3359cf42
Alim Mambetov <alim.mambetov.8866@gmail.com>
2025-11-26 15:23:34 +0300

    change port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import app from '@/app';
 import { connectDB } from '@/config/database';
 
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = 3030;
 
 async function start() {
 	await connectDB();
